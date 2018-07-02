a759ab6a8bfe2013eca9d437bf51a77a8b56b043
Luke Parker <lukep@digispend.io>
2018-07-01 20:04:00 -0500

    Delete token/user cookies before logging in

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,6 @@
+document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+document.cookie = "user=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+
 async function login() {
     window.users.login(document.getElementById("user").value, document.getElementById("pass").value);
 }
